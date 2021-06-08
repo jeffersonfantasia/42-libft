72938dc801aeadfec898e0b720d28f7050b7354e
jfranchi <jefferson.fantasia@gmail.com>
2021-06-08 18:24:06 -0300

    create and update files

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jfranchi <jfranchi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/08 13:37:14 by jfranchi          #+#    #+#             */
-/*   Updated: 2021/06/08 17:38:59 by jfranchi         ###   ########.fr       */
+/*   Updated: 2021/06/08 18:20:50 by jfranchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ static	char	**_split_words(char *str, char c)
 	unsigned int	count_words;
 	char			**split_words;
 
+	count_words = 0;
 	while (*str)
 	{
 		if (*str != c)
@@ -25,7 +26,8 @@ static	char	**_split_words(char *str, char c)
 				str++;
 			count_words++;
 		}
-		str++;
+		if (*str)
+			str++;
 	}
 	split_words = (char **)ft_calloc(count_words + 1, sizeof(char *));
 	if (split_words == NULL)
@@ -46,40 +48,41 @@ static	void	_free_all(char **split)
 	free(split);
 }
 
-static	void	_char_words(char *str, char *splited_str, size_t len)
+static	void	_char_words(char *dest, char *src, size_t len)
 {
 	while (len--)
 	{
-		*splited_str = *str;
-		splited_str++;
-		str++;
+		*dest = *src;
+		dest++;
+		src++;
 	}
 }
 
-static	char	**_result_splited(char *s, char c, char **splited, size_t pos)
+static	char	**_result_splited(char *s, char c, char **splited)
 {
-	size_t	count;
+	size_t	count[2];
 
+	count[1] = 0;
 	while (*s)
 	{
-		count = 0;
 		if (*s != c)
 		{
+			count[0] = 0;
 			while (*s != c && *s)
 			{
 				s++;
-				count++;
+				count[0]++;
 			}
-			splited[pos] = (char *)ft_calloc((count + 1), sizeof(char));
-			if (splited[pos] == NULL)
+			splited[count[1]] = (char *)ft_calloc((count[0] + 1), sizeof(char));
+			if (splited[count[1]] == NULL)
 			{
 				_free_all(splited);
 				return (NULL);
 			}
-			_char_words((char *)(s - count), splited[pos], count);
-			pos++;
+			_char_words(splited[count[1]++], (s - count[0]), count[0]);
 		}
-		s++;
+		if (*s)
+			s++;
 	}
 	return (splited);
 }
@@ -87,10 +90,15 @@ static	char	**_result_splited(char *s, char c, char **splited, size_t pos)
 char	**ft_split(char const *s, char c)
 {
 	char	**splited;
-	size_t	pos;
 
-	pos = 0;
 	splited = _split_words((char *)s, c);
-	splited = _result_splited((char *)s, c, splited, pos);
+	if (splited == NULL)
+		return (NULL);
+	splited = _result_splited((char *)s, c, splited);
+	if (splited == NULL)
+	{
+		free(splited);
+		return (NULL);
+	}
 	return (splited);
 }
