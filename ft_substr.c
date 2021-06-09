a1fa08db9dfd646703a91245993546b5fddc0da4
jfranchi <jefferson.fantasia@gmail.com>
2021-06-09 19:54:44 -0300

    create and update files

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jfranchi <jfranchi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/04 14:53:57 by jfranchi          #+#    #+#             */
-/*   Updated: 2021/06/07 18:37:50 by jfranchi         ###   ########.fr       */
+/*   Updated: 2021/06/09 19:51:42 by jfranchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,26 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char			*str;
 	unsigned int	i;
-	size_t			s_len;
 
 	if (!s)
 		return (NULL);
-	str = (char *)ft_calloc((len + 1), sizeof(char));
+	if (start > ft_strlen(s))
+	{
+		str = (char *)ft_calloc(1, sizeof(char));
+		if (str == NULL)
+			return (NULL);
+		return (str);
+	}
+	if (len > ft_strlen(s + start))
+		str = (char *)ft_calloc((ft_strlen(s + start) + 1), sizeof(char));
+	else
+		str = (char *)ft_calloc((len + 1), sizeof(char));
 	if (str == NULL)
 		return (NULL);
-	s_len = ft_strlen(s);
-	if (start >= s_len)
-		s = (s + s_len);
-	else
-		s = (s + start);
 	i = 0;
-	while (len-- && *(s + i))
+	while (i < len)
 	{
-		*(str + i) = *(s + i);
+		str[i] = s[start + i];
 		i++;
 	}
 	return (str);
