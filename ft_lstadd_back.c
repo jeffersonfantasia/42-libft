f48d5e673a23470cb7269e2f68bcb2e6ae2b625b
jfranchi <jefferson.fantasia@gmail.com>
2021-06-22 19:59:10 -0300

    create and update files

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: jfranchi <jfranchi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/17 19:18:39 by jfranchi          #+#    #+#             */
-/*   Updated: 2021/06/21 17:48:22 by jfranchi         ###   ########.fr       */
+/*   Updated: 2021/06/22 19:50:18 by jfranchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,11 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*tmp;
 
-	new->next = NULL;
 	if (*lst == NULL)
 		*lst = new;
 	else
 	{
-		tmp = *lst;
-		while (tmp->next != NULL)
-			tmp = tmp->next;
+		tmp = ft_lstlast(*lst);
 		tmp->next = new;
 	}
 }
