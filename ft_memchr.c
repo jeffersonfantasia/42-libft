436447fb3006c68a3750ae2d323309b3343464f9
jfranchi <jefferson.fantasia@gmail.com>
2021-06-25 14:13:50 -0300

    create or upload files

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: jfranchi <jfranchi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/02 20:35:55 by jfranchi          #+#    #+#             */
-/*   Updated: 2021/06/02 20:54:08 by jfranchi         ###   ########.fr       */
+/*   Updated: 2021/06/25 14:13:05 by jfranchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,14 @@
 
 void	*ft_memchr(const void *p_str, int c, size_t len)
 {
-	unsigned char	*str;
+	size_t	i;
 
-	str = (unsigned char *)p_str;
-	if (!str)
-		return (NULL);
-	while (len--)
+	i = 0;
+	while (i < len)
 	{
-		if ((*str) == (unsigned char)c)
-			return (str);
-		str++;
+		if (((unsigned char *)p_str)[i] == (unsigned char)c)
+			return (((unsigned char *)p_str) + i);
+		i++;
 	}
 	return (NULL);
 }
